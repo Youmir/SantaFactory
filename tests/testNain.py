d5e57fd29ad4243a20ed96b6b91e6293e9072b6a
Youmir <s.youmir@gmail.com>
2020-02-23 18:41:24 +0100

    adding unittest for Nain

---
+++ tests/testNain.py
@@ -0,0 +1,18 @@
+import unittest
+from .classes.nain import Nain
+from .classes.cadeau import Cadeau
+
+class NainTest(unittest.TestCase):
+
+    def TestStartWrapGift(self):
+        gift = Cadeau("small")
+        worker = Nain(1)
+        worker.startWrapGift(gift)
+        self.assertEqual(worker.IsDispo, False)
+        self.assertEquals(worker.dispoAfter,0.5)
+
+
+
+
+if __name__ == "__main__":
+    unittest.main()
